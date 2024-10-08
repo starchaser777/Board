af85a8b4bf1d8e027dca07adfdc558d3aba4bf62
starchaser777 <les980730@gmail.com>
2024-10-08 16:36:10 +0900

    Controller: 웹에서 기능실행, Templates: 각 기능(목록, 글등록, 글조회, 글수정, 글삭제)별 html 페이지 작성

---
+++ src/test/java/kr/ac/kopo/board/repository/ReplyRepositoryTests.java
@@ -0,0 +1,34 @@
+package kr.ac.kopo.board.repository;
+
+import kr.ac.kopo.board.entity.Board;
+import kr.ac.kopo.board.entity.Reply;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class ReplyRepositoryTests {
+    @Autowired
+    private ReplyRepository replyRepository;
+
+    @Test
+    public void insertReply(){
+        IntStream.rangeClosed(1, 300).forEach(i ->{
+            long bno = (long)(Math.random() * 100 + 1); //1~100 임의의 long형의 정수 값
+
+            Board board = Board.builder()
+                    .bno(bno)
+                    .build();
+
+            Reply reply = Reply.builder()
+                    .text("Reply "+i)
+                    .board(board)
+                    .replyer("guest")
+                    .build();
+
+            replyRepository.save(reply);
+        });
+    }
+}
